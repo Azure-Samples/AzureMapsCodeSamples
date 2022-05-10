113a30ad409a8ebb7eb4eb3647ef9179b93a08ac
Ricky Brundritt <ricky_brundritt@hotmail.com>
2022-05-10 12:56:22 -0700

    Optimize parcel.json file and other minor updates
     - Optimize parcels.json file (fix ring orientation, removed some properties, reduced coordinates to 6 decimal places).
    - Fix broken image URL path in sample.
    - Add style control to measuring sample. Its nice to be able to measure with satellite imagery.

--- Samples/Spatial Analysis/GeoJSON Routing/index.js
+++ Samples/Spatial Analysis/GeoJSON Routing/index.js
@@ -65,7 +65,7 @@ function GetMap() {
 
         //Create draggable HTML markers for the end points. They drag much smoother than symbols. 
         var fromImg = document.createElement('img');
-        fromImg.src = '/Common/images/icons/startPin.png';
+        fromImg.src = '/images/icons/startPin.png';
         fromImg.id = 'from';
 
         fromPin = new atlas.HtmlMarker({
@@ -83,7 +83,7 @@ function GetMap() {
         map.markers.add(fromPin);
 
         var toImg = document.createElement('img');
-        toImg.src = '/Common/images/icons/endPin.png';
+        toImg.src = '/images/icons/endPin.png';
         toImg.id = 'to';        
 
         toPin = new atlas.HtmlMarker({
