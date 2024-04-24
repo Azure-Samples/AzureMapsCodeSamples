f5fdc2fe56c23a6afdeea45682c2d49709141ea7
Clemens Schotte <clemens@navatron.com>
2024-04-24 23:34:04 +0200

    REST APIs version update

--- Static/lib/azure-maps/azure-maps-helper.js
+++ Static/lib/azure-maps/azure-maps-helper.js
@@ -1,18 +1,10 @@
 ﻿/**
-* This is a reusable function that sets the Azure Maps platform domain,
-* signs the request, and makes use of any transformRequest set on the map.
-* Use like this: `const data = await processRequest(url);`
-*/
+ * This is a reusable function that sets the Azure Maps platform domain,
+ * signs the request, and makes use of any transformRequest set on the map.
+ * Use like this: `const data = await processRequest(url);`
+ */
 async function processRequest(url) {
-    // Replace the domain placeholder to ensure the same Azure Maps is used throughout the app.
-    url = url.replace('{azMapsDomain}', atlas.getDomain());
-
-    // Get the authentication details from the map for use in the request.
-    var requestParams = map.authentication.signRequest({ url: url });
-
-    // Transform the request.
-    var transform = map.getServiceOptions().transformRequest;
-    if (transform) requestParams = transform(url);
+    const requestParams = await signRequest(url);
 
     const response = await fetch(requestParams.url, {
         method: 'GET',
@@ -25,4 +17,32 @@ async function processRequest(url) {
     }
 
     return response.json();
+}
+
+async function processPostRequest(url, body) {
+    const requestParams = await signRequest(url);
+
+    return fetch(requestParams.url, {
+        method: 'POST',
+        mode: 'cors',
+        headers: new Headers(requestParams.headers),
+        body: body
+    });
+}
+
+async function signRequest(url) {
+    // Replace the domain placeholder to ensure the same Azure Maps is used throughout the app.
+    url = url.replace('{azMapsDomain}', atlas.getDomain());
+
+    // Get the authentication details from the map for use in the request.
+    var requestParams = await map.authentication.signRequest({ url });
+
+    // Add content type of body to the headers.
+    requestParams.headers['Content-type'] = 'application/json; charset=UTF-8';
+
+    // Transform the request.
+    var transform = map.getServiceOptions().transformRequest;
+    if (transform) requestParams = await transform(url);
+
+    return requestParams;
 }
